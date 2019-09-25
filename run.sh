0efa3ceefb6223d6d0ed104a4ca111e8fb67ed33
Le Huy Hoang <lehoang318@gmail.com>
2019-09-25 21:58:43 +0700

    Update run.sh to detect environment

--- run.sh
+++ run.sh
@@ -1,12 +1,30 @@
 #!/bin/sh
 
+# Detect environment
+case "$OSTYPE" in
+    linux*)
+        alias PYRCC=pyrcc5
+        alias PYUIC=pyuic5
+        ;;
+    bsd*)
+        alias PYRCC=pyrcc5
+        alias PYUIC=pyuic5
+        ;;
+    msys*)
+        alias PYRCC=pyrcc5
+        alias PYUIC=pyuic5.bat
+        ;;
+    *)
+        echo "unknown: $OSTYPE" ;;
+esac
+
 # Clean-up
 rm -f editor.py main_window.py icons_rc.py *.pyc *.bak
 
 # Generate resource & UI
-pyrcc5 icons.qrc -o icons_rc.py
-pyuic5.bat ./ui/editor.ui -o editor.py
-pyuic5.bat ./ui/main_window.ui -o main_window.py
+PYRCC icons.qrc -o icons_rc.py
+PYUIC ./ui/editor.ui -o editor.py
+PYUIC ./ui/main_window.ui -o main_window.py
 
 # Execute
 python -m main
