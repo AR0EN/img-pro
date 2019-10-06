b5957af7ad7a20ecfd82df8422ffb42a8c0d5bb1
Le Huy Hoang <lehoang318@gmail.com>
2019-10-06 20:52:25 +0700

    Implement Image Rotation:
    - Change constructor of Image class
    - Change structure of MainWindow, EditorWindow classes
      + Parent: QtWidgets.QMainWindow
    - Change title of EditorWindow
    - Add new attribute to EditorWindow: editingImage
    - Add new method to MainWindow: terminateEditorWindow()

--- editor_window.py
+++ editor_window.py
@@ -1,15 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 02 22:01:00 2019
+
+@author: Nguyen Tien Nam
+"""
+
+import sys
+import os
 
 from PyQt5 import QtWidgets
 from ui_editor_window import Ui_EditorWindow
 from wrapper import CommonFunctions
+from images import Image
+from log import LOG
 
-
-class EditorWindow():
-
-    def __init__(self, imported_image):
-        self.editorWindow = QtWidgets.QMainWindow()
+class EditorWindow(QtWidgets.QMainWindow):
+    def __init__(self, _mainWindow, imported_image):
+        # Initialize attributes of QtWidgets.QMainWindow
+        if 3 > sys.version_info[0]:
+            # Python 2
+            super(EditorWindow, self).__init__()
+            
+        else:
+            # Python 3
+            super().__init__()
+        
+        self.mainWindow = _mainWindow
         self.imported_image = imported_image
-        self.editor = Ui_EditorWindow()
-        self.editor.setupUi(self.editorWindow)
-        CommonFunctions.display(self.imported_image, self.editor.labelCanvas)
-        self.editorWindow.show()
+        self.editingImage = Image(Image.BY_DATA, imported_image.data)
+        
+        self.ui = Ui_EditorWindow()
+        self.ui.setupUi(self)
+        # Set title
+        imageFileName = os.path.basename(imported_image.filePath).split('.')[0]
+        self.setWindowTitle(imageFileName)
+        
+        CommonFunctions.display(self.editingImage, self.ui.labelCanvas)
+        self.show()
+        
+    def closeEvent(self, event):
+        LOG("[EditorWindow.closeEvent()] Closing " + self.windowTitle() + " Window")
+        # Notify MainWindow
+        self.mainWindow.terminateEditorWindow(self)
+        
+        # Terminate sub-attributes/processes
+        
+        # Call QtWidgets.QMainWindow 's procedure
+        if 3 > sys.version_info[0]:
+            # Python 2
+            super(EditorWindow, self).closeEvent(event)
+            
+        else:
+            # Python 3
+            super().closeEvent(event)
+            
--- main_window.py
+++ main_window.py
@@ -1,3 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 13 20:55:37 2019
+
+@author: Le Huy Hoang
+"""
+
+import sys
 
 from PyQt5 import QtWidgets
 from images import Images
@@ -5,13 +13,22 @@ from ui_main_window import Ui_MainWindow
 from editor_window import EditorWindow
 from wrapper import CommonFunctions
 
-class MainWindow():
+class MainWindow(QtWidgets.QMainWindow):
     SUPPORT_FORMAT = '(*.bmp *.gif *.jpg *.jpeg *.png *.pbm *.pgm *.ppm *.xbm *.xpm *.svg)'
 
     def __init__(self):
-        self.mainWindow = QtWidgets.QMainWindow()
+        # Initialize attributes of QtWidgets.QMainWindow
+        if 3 > sys.version_info[0]:
+            # Python 2
+            super(MainWindow, self).__init__()
+            
+        else:
+            # Python 3
+            super().__init__()
+            
         self.gui = Ui_MainWindow()
-        self.gui.setupUi(self.mainWindow)
+        #self.gui.setupUi(self.mainWindow)
+        self.gui.setupUi(self)
         self.subWindows = []
         self.imgList = Images(self.gui.listWidgetImgList)
         
@@ -20,7 +37,7 @@ class MainWindow():
         self.gui.actionEdit.triggered.connect(self.actionEditClickEvt)
         self.gui.actionExit.triggered.connect(self.actionExitClickEvt)
         self.gui.listWidgetImgList.currentItemChanged.connect(self.actionCurrentItemChangedEvt)
-        self.mainWindow.show()
+        self.show()
     
     # Display Current Item when it changed
     def actionCurrentItemChangedEvt(self):
@@ -35,9 +52,13 @@ class MainWindow():
     
     # Edit selected image
     def actionEditClickEvt(self):
-        editor = EditorWindow(self.imgList.images[self.gui.listWidgetImgList.currentRow()])
-        self.subWindows.append(editor.editorWindow)
-
+        editor = EditorWindow(self, self.imgList.images[self.gui.listWidgetImgList.currentRow()])
+        self.subWindows.append(editor)
+    
+    # Terminate an Editor Window
+    def terminateEditorWindow(self, editorWindow):
+        self.subWindows.remove(editorWindow)
+    
     # Exit
     def actionExitClickEvt(self):
-        self.mainWindow.close()
+        self.close()
