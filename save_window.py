1891722490c9a2cfa82c83aa878d761e9da09495
ntnam <aclong28@gmail.com>
2019-10-19 18:18:33 +0700

    resize-image: add option to resize with percentage

--- save_window.py
+++ save_window.py
@@ -10,25 +10,46 @@ import numpy as np
 class SaveWindow():
 
     def __init__(self, imageInfo):
+        self.imageResized = None
         self.saveWindow = QtWidgets.QMainWindow()
         self.imageInfo = imageInfo
         self.save = Ui_SaveWindow()
         self.save.setupUi(self.saveWindow)
-        self.height, self.width, _ = imageInfo.displayData.shape
-        self.save.sbWidth.setValue(self.width)
-        self.save.sbHeight.setValue(self.height)
+        self.save.sbWidth.setValue(self.imageInfo.displayData.shape[1])
+        self.save.sbHeight.setValue(self.imageInfo.displayData.shape[0])
         self.save.btnSave.clicked.connect(self.onClickSave)
+        self.save.rbWiHe.clicked.connect(self.enableWiHe)
+        self.save.rbPercent.clicked.connect(self.enablePercent)
         self.saveWindow.show()
 
     def onClickSave(self):
-        self.height = self.save.sbHeight.value()
-        self.width = self.save.sbWidth.value()
-        dim = (self.width, self.height)
-        resized = cv2.resize(self.imageInfo.displayData, dim, interpolation = cv2.INTER_AREA)
-        resizedRGB = cv2.cvtColor(resized, cv2.COLOR_BGR2RGB)
+        imageResized = self.resizeImage()
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
         fileName, _ = QFileDialog.getSaveFileName(None,"Save Image File","","Image Files (*.bmp *.dib *.jpeg *.jpg *.png *.pbm *.pgm *.ppm *.tiff *.tif)", options=options)
-        cv2.imwrite(fileName, resizedRGB)
+        cv2.imwrite(fileName, imageResized)
+
+    def enableWiHe(self):
+        self.save.sbPercent.lineEdit().setEnabled(False)
+        self.save.sbWidth.lineEdit().setEnabled(True)
+        self.save.sbHeight.lineEdit().setEnabled(True)
+
+    def enablePercent(self):
+        self.save.sbWidth.lineEdit().setEnabled(False)
+        self.save.sbHeight.lineEdit().setEnabled(False)
+        self.save.sbPercent.lineEdit().setEnabled(True)
+
+    def resizeImage(self):
+        if self.save.rbPercent.isChecked():
+            scale_percent = self.save.sbPercent.value()
+            width = int(self.imageInfo.displayData.shape[1] * scale_percent / 100)
+            height = int(self.imageInfo.displayData.shape[0] * scale_percent / 100)
+        elif self.save.rbWiHe.isChecked():
+            width = self.save.sbWidth.value()
+            height = self.save.sbHeight.value()
+        dim = (width, height)
+        resized = cv2.resize(self.imageInfo.displayData, dim, interpolation = cv2.INTER_AREA)
+        resizedRGB = Image.convertToRGBU8(resized)
+        return resizedRGB
 
 
